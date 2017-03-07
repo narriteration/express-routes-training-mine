7a3998fc0574a13a97a8ff8a34cf51fc43b3845a
narriteration <narriteration@gmail.com>
2017-03-06 20:34:38 -0800

    created root route that sends index.html file from views dir

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // REQUIREMENTS
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
 
 // MIDDLEWARE
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 //ROUTES (FILL THIS IN)
 // Create a root route that responds by sending the index.html file from the views directory.
 
 app.get('/', function rootDir(req, res) {
-    res.send("Chelsea!");
+    res.sendFile('./views/index.html', {root: __dirname});
 })
 
 // SERVER START
