70aef1255cf900177240387d05661174f5c02069
narriteration <narriteration@gmail.com>
2017-03-06 20:55:19 -0800

    completed number guessing game with help from soln code

--- server.js
+++ server.js
@@ -15,9 +15,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function rootDir(req, res) {
     res.sendFile('./views/index.html', {root: __dirname});
-})
+});
+
+// GUESS MY NUMBER GAME
+var targetNumber = 20;
+
+app.get('/pick-a-number', function(req, res){
+    var num = parseInt(req.query.number);
+    if (num === targetNumber){
+      res.send('Nailed it!');
+    } else if (num > targetNumber) {
+      res.send('Too High!');
+    } else if (num < targetNumber) {
+      res.send('Too Low.');
+    } else {
+      console.log('Something odd happened! target:', targetNumber, typeof(targetNumber),'num: ', num, typeof(num));
+      res.send('Your guess is beyond compare!');
+    }
+});
+
+app.post('/pick-a-number', function(req, res) {
+    targetNumber = parseInt(req.body.number);
+    response.status(418).send('Number updated successfully!');
+});
 
 // SERVER START
-app.listen(4000, function(){
-    console.log("Localhost listening at localhost: 4000");
+app.listen(3000, function(){
+    console.log("Localhost listening at localhost: 3000");
 });
